a28449e3ca40db02407833a43faef6d1e8367897
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-10 22:09:27 -0300

    Clase 3 10-7

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+
+	edad=document.getElementById("edad").value;
+
+	if (edad>=18)
+	 {
+	  alert("es mayor de edad")
+	}
+	else if (edad<13) 
+	{
+		alert("es menor de edad")
+	}else 
+		{
+			alert("es adolescente")
+		}	
+
+		
 
 
 
