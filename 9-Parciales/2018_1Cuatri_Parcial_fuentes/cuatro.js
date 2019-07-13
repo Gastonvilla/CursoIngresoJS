487af5cbe42ba773295da9dbff0e04d69b39fba0
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-12 22:04:10 -0300

    Clase 12-7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+	var numeroUno;
+
+	var numeroDos;
+
+	var resultado;
+
+	numeroUno=prompt("numeroUno");
+
+	numeroDos=prompt("numeroDos");
+
+		if(numeroUno == numeroDos){
+
+
+		resultado=(numeroUno+numeroDos);
+	
+	}else if(numeroUno>numeroDos)
+	
+
+		resultado=numeroUno-numeroDos;
+
+		{
+	}else if (numeroUno<numeroDos)
+
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+		resultado=numeroUno+numeroDos;
+
+	 {
+
+	 }alert(resultado);
+
 
 }
