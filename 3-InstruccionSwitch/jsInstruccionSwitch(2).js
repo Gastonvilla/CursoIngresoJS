1bd1ef885d1c305e88202caa709a809c832559e2
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-17 22:07:08 -0300

    Clase 17-7

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,36 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño){
+
+	case "Julio":
+	case "Agosto":
+
+	mensaje=("Abrigate que hace frio");
+	break;
+
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+
+	mensaje=("Falta para el invierno");
+	break;
+
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	case "Enero":
+	case "Febrero":
+
+	mensaje=("Ya pasamos el frio, ahora el calor");
+
+}
+
+
+
+alert (mensaje);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,7 +3,21 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño){
+
+	case "Febrero":
+
+	mensaje="Este mes no tiene mas de 29 dias";
+
+	break;
+
+	default:
+
+	mensaje="Este mes tiene 30 dias o mas";
+	break;
+}
+
+alert (mensaje);
 	
 	
 
