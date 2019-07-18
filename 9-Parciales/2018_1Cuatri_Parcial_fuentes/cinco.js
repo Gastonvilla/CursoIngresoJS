7ef1e89910e65a9646c6d2c1f296499f74367799
Gastonvilla <52511988+Gastonvilla@users.noreply.github.com>
2019-07-17 22:10:06 -0300

    Clase 17-7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+var planeta;
 
+planeta=prompt("planeta");
+
+switch(planeta){
+	case "tierra":
+
+	mensaje="aca vivimos";
+	break;
+
+	case "mercurio":
+	case "venus":
+
+	mensaje="aca hace mas calor";
+	break;
+
+	case "marte":
+	case "jupiter":
+	case "saturno":
+	case "urano":
+	case "neptuno":
+
+	mensaje="aca hace frio";
+	break;
+
+	default:
+	mensaje="no es planeta valido";
+	break;
+
+
+
+}
+	alert(mensaje);
 }
